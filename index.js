d5137537ede993b6b71f166c8c101ecec099a1f2
Ryan Adams <radams@li-fvfgl0ppq6lx-macbookair.attlocal.net>
2021-11-11 16:32:36 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const inquirer = require("inquirer");
+
+function init() {
+    console.log('Welcome To Hasher. This application will output on of, if not all three, of the following hashes: MD5, SHA1, SHA2');
+    hashData();
+}
+
+function hashData() {
+
+    inquirer.prompt([{
+
+    }])
+
+}
